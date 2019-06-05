6c27b45d356a397e206ea6bbc4621f7f23bb5579
elenapitul <elenapitul@gmail.com>
2019-06-06 00:55:40 +0300

    first try of the last problem

---
+++ Strings_advanced/4_SantaHo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _4_SantaHo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Ho("")); //Ho!
+            Console.WriteLine(Ho(Ho(""))); //Ho Ho!
+            Console.WriteLine(Ho(Ho(Ho("")))); //Ho Ho Ho!
+        }
+                
+        public static string Ho(string ho)
+        {
+            if(ho == "")
+            {
+                return "Ho!";
+            } else if (ho == "Ho!")
+            {
+                return "Ho Ho!";
+            } else if (ho == "Ho Ho!")
+            {
+                return "Ho Ho Ho!";
+            }
+            else
+            {
+                return "";
+            }           
+        }           
+
+    }
+}
