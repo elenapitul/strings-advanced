19c884d36f94d150dd77a2581036fec7a817d560
elenapitul <elenapitul@gmail.com>
2019-06-05 21:27:55 +0300

    adding another problem

---
+++ Strings_advanced/3_LikeLike/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _3_LikeLike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] empty = new string[0];
+            Like(empty);
+
+            string[] onename = { "Peter" };
+            Like(onename);
+
+            string[] twonames = { "Jacob", "Alex" };
+            Like(twonames);
+
+            string[] threenames = { "Max", "John", "Mark" };
+            Like(threenames);
+
+            string[] fournames = { "Alex", "Jacob", "Mark", "Max" };
+            Like(fournames);
+
+        }
+        public static void Like(string[] names)
+        {
+            if (names.Length == 0)
+            {
+                Console.WriteLine("No one likes this");
+            }
+            else if (names.Length == 1)
+            {
+                Console.WriteLine($"{names[0]} likes this");
+            }
+            else if (names.Length == 2)
+            {
+                Console.WriteLine($"{names[0]} and {names[1]} like this");
+            }
+            else if (names.Length == 3)
+            {
+                Console.WriteLine($"{names[0]}, {names[1]} and {names[2]} like this");
+            }
+            else if (names.Length >= 4)
+            {
+                Console.WriteLine($"{names[0]}, {names[1]} and {names.Length - 2} others like this.");
+            }            
+        }
+    }
+}
