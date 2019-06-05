9dc892de493964b5ad9d73267becd3038e58049a
elenapitul <elenapitul@gmail.com>
2019-06-05 20:21:50 +0300

    add first 2 problems

---
+++ Strings_advanced/1_Middle Character/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+namespace _1_Middle_Character
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Return the middle character of the word.
+            //If the word's length is odd, return the middle character.
+            //If the word's length is even, return the middle 2 characters.
+            //Should work for words of 999 characters.
+
+            Console.WriteLine(MiddleChar("test"));
+            Console.WriteLine();
+
+            Console.WriteLine(MiddleChar("testing"));
+            Console.WriteLine();
+
+            Console.WriteLine(MiddleChar("middle"));
+            Console.WriteLine();
+
+            Console.WriteLine(MiddleChar("A"));
+            //https://www.lettercount.com/;
+            Console.WriteLine();
+
+            Console.WriteLine(MiddleChar("ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"));
+            Console.WriteLine();
+            Console.WriteLine(MiddleChar("sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"));
+            Console.WriteLine();            
+
+        }
+
+        public static string MiddleChar(string mid)
+        {
+            if (mid.Length == 1)
+            {
+                return ($"The string {mid} is also the middle.");
+                //mid;
+            }
+            else if (mid.Length < 1000)
+            {
+                if (mid.Length % 2 == 0)
+                {
+                    return ($"The string {mid} has the middle characters: {(mid.Substring((mid.Length / 2 - 1), 2))}.");
+                    //(mid.Substring((mid.Length / 2 - 1), 2));
+                }
+                else
+                {
+                    return ($"The string {mid} has the middle character: {(mid.Substring(mid.Length / 2, 1))}."); 
+                    //(mid.Substring(mid.Length / 2, 1));
+                }
+            }
+            else
+            {
+                return ("This string has more than 1000 characters.");
+            }
+        }
+
+    }
+
+}
---
+++ Strings_advanced/2_Kata_CountChars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _2_Kata_CountChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Count the number of times that character appears in the string.
+            //Case insensitive.
+            Console.WriteLine(CountChars("fizzbuzz", 'z'));
+            Console.WriteLine();
+            Console.WriteLine(CountChars("Fancy fifth fly aloof", 'f'));
+            Console.WriteLine();
+            Console.WriteLine(CountChars("123456789012345612345123",'2'));
+            Console.WriteLine();
+        }
+
+        public static object CountChars(string s, char c)
+        {
+            int count = 0;
+            foreach (char i in s)
+            {
+                if(char.ToUpper(i) == char.ToUpper(c))
+                {
+                    count++;
+                }
+            }
+            return $"The character {c} can be found in the string {s} {count} times.";
+        }
+    }
+}
